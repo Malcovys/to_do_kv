1ae4da2c0187d85be45acefa14268bceafb4eddf
Malcovys <malcovys22.aps@gmail.com>
2022-10-22 16:47:25 +0300

    Creat graphical interface

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Widget
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class mainInteface(Widget):
+    pass
+
+# App core
+class To_Do_PyApp(App):
+    pass
+
+To_Do_PyApp().run()
